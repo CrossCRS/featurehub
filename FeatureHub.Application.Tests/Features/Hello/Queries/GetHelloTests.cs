e42e29d43018d6a5217c849cb518b60207d21657
Norbert Budzynski <cross@linux.pl>
2026-04-30 11:20:41 +0200

    Add xUnit

---
+++ FeatureHub.Application.Tests/Features/Hello/Queries/GetHelloTests.cs
@@ -0,0 +1,18 @@
+﻿using FeatureHub.Application.Features.Hello.Queries;
+
+namespace FeatureHub.Application.Tests.Features.Hello.Queries;
+
+public class GetHelloTests
+{
+    [Fact]
+    public async Task GetHello_ReturnsHello()
+    {
+        var handler = new GetHelloQueryHandler();
+        var query = new GetHelloQuery();
+
+        var result = await handler.ExecuteAsync(query);
+
+        Assert.NotNull(result);
+        Assert.Equal("Hello from Darker!", result);
+    }
+}
