ff74950d5d6a3b549a50052611fd7dfb2169389c
Norbert Budzynski <cross@linux.pl>
2026-04-30 20:59:42 +0200

    Add AuthController

---
+++ FeatureHub.Api/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using FeatureHub.Api.Models.Auth;
+using FeatureHub.Application.Auth.Commands.Login;
+using Microsoft.AspNetCore.Mvc;
+using Paramore.Brighter;
+
+namespace FeatureHub.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly IAmACommandProcessor _commandProcessor;
+
+    public AuthController(IAmACommandProcessor commandProcessor)
+    {
+        _commandProcessor = commandProcessor;
+    }
+
+    [Route("login")]
+    [HttpPost]
+    public async Task<IActionResult> Login([FromBody] LoginRequest loginRequest)
+    {
+        var command = new LoginCommand(loginRequest.Username, loginRequest.Password);
+        await _commandProcessor.SendAsync(command);
+
+        return Ok(new { command.Token });
+    }
+}
