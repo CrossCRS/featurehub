abff76d837037cd5cfc4fdb4918c25a5a2a6f64e
Norbert Budzynski <cross@linux.pl>
2026-05-09 18:53:10 +0200

    Add CreateProject POST endpoint

--- FeatureHub.Api/Controllers/ProjectsController.cs
+++ FeatureHub.Api/Controllers/ProjectsController.cs
@@ -1,7 +1,10 @@
-﻿using FeatureHub.Application.Common.Interfaces.Identity;
+﻿using FeatureHub.Api.Models.Projects;
+using FeatureHub.Application.Common.Interfaces.Identity;
+using FeatureHub.Application.Projects.Commands.CreateProject;
 using FeatureHub.Application.Projects.Queries.GetProjectsByOwner;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Paramore.Brighter;
 using Paramore.Darker;
 
 namespace FeatureHub.Api.Controllers;
@@ -13,11 +16,13 @@ public class ProjectsController : ControllerBase
 {
     private readonly ICurrentUserService _currentUserService;
     private readonly IQueryProcessor _queryProcessor;
+    private readonly IAmACommandProcessor _commandProcessor;
 
-    public ProjectsController(ICurrentUserService currentUserService, IQueryProcessor queryProcessor)
+    public ProjectsController(ICurrentUserService currentUserService, IQueryProcessor queryProcessor, IAmACommandProcessor commandProcessor)
     {
         _currentUserService = currentUserService;
         _queryProcessor = queryProcessor;
+        _commandProcessor = commandProcessor;
     }
 
     [HttpGet]
@@ -27,5 +32,18 @@ public class ProjectsController : ControllerBase
         var result = await _queryProcessor.ExecuteAsync(new GetProjectsByOwner(userId));
 
         return result;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateProject([FromBody] CreateProjectRequest request)
+    {
+        var userId = _currentUserService.GetUserId();
+
+        var command = new CreateProjectCommand(userId, request.Name);
+
+        await _commandProcessor.SendAsync(command);
+
+        // TODO: CreatedAt
+        return NoContent();
     }
 }
---
+++ FeatureHub.Api/Models/Projects/CreateProjectRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace FeatureHub.Api.Models.Projects;
+
+public class CreateProjectRequest
+{
+    public required string Name { get; set; }
+}
