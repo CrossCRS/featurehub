05610f6c50f78b48defc3f52686e861df73fadc2
Norbert Budzynski <crosscrs@outlook.com>
2026-05-24 17:16:20 +0200

    Add FeatureFlag queries

---
+++ FeatureHub.Application/FeatureFlags/Queries/GetFeatureFlagById/GetFeatureFlagByIdValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace FeatureHub.Application.FeatureFlags.Queries.GetFeatureFlagById;
+
+public class GetFeatureFlagByIdValidator : AbstractValidator<GetFeatureFlagById>
+{
+    public GetFeatureFlagByIdValidator()
+    {
+        RuleFor(x => x.ProjectId)
+            .GreaterThan(0).WithMessage("Project ID must be greater than 0.");
+
+        RuleFor(x => x.EnvironmentId)
+            .GreaterThan(0).WithMessage("Environment ID must be greater than 0.");
+
+        RuleFor(x => x.FeatureFlagId)
+            .GreaterThan(0).WithMessage("Feature Flag ID must be greater than 0.");
+    }
+}
---
+++ FeatureHub.Application/FeatureFlags/Queries/GetFeatureFlagsByEnvironment/GetFeatureFlagsByEnvironmentValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace FeatureHub.Application.FeatureFlags.Queries.GetFeatureFlagsByEnvironment;
+
+public class GetFeatureFlagsByEnvironmentValidator : AbstractValidator<GetFeatureFlagsByEnvironment>
+{
+    public GetFeatureFlagsByEnvironmentValidator()
+    {
+        RuleFor(x => x.ProjectId)
+            .GreaterThan(0).WithMessage("Project ID must be greater than 0.");
+
+        RuleFor(x => x.EnvironmentId)
+            .GreaterThan(0).WithMessage("Environment ID must be greater than 0.");
+    }
+}
