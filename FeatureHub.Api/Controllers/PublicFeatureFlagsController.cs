34be43e235fb27d089a9905c088645bc4f2beee6
Norbert Budzynski <crosscrs@outlook.com>
2026-05-25 12:03:08 +0200

    Add Public Feature Flags controller

---
+++ FeatureHub.Api/Controllers/PublicFeatureFlagsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Paramore.Darker;
+
+namespace FeatureHub.Api.Controllers;
+
+[AllowAnonymous]
+[Route("api/public")]
+[ApiController]
+public class PublicFeatureFlagsController : ControllerBase
+{
+    private readonly IQueryProcessor _queryProcessor;
+
+    public PublicFeatureFlagsController(IQueryProcessor queryProcessor)
+    {
+        _queryProcessor = queryProcessor;
+    }
+
+    [HttpGet("{environmentToken}")]
+    public async Task<IActionResult> GetPublicFeatureFlags([FromRoute] string environmentToken, [FromQuery] string? clientHash)
+    {
+        throw new NotImplementedException();
+    }
+}
