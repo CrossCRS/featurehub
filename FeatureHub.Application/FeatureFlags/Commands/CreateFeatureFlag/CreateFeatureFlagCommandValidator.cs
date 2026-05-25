ad223ec654305d68d75c4d16646b120179edeec3
Norbert Budzynski <crosscrs@outlook.com>
2026-05-25 10:49:15 +0200

    Add CreateFeatureFlag command

---
+++ FeatureHub.Application/FeatureFlags/Commands/CreateFeatureFlag/CreateFeatureFlagCommandValidator.cs
@@ -0,0 +1,25 @@
+using FluentValidation;
+
+namespace FeatureHub.Application.FeatureFlags.Commands.CreateFeatureFlag;
+
+public class CreateFeatureFlagCommandValidator : AbstractValidator<CreateFeatureFlagCommand>
+{
+    public CreateFeatureFlagCommandValidator()
+    {
+        RuleFor(x => x.ProjectId)
+            .GreaterThan(0).WithMessage("Project ID must be greater than 0.");
+
+        RuleFor(x => x.EnvironmentId)
+            .GreaterThan(0).WithMessage("Environment ID must be greater than 0.");
+
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Feature flag name is required.")
+            .MaximumLength(100).WithMessage("Feature flag name must not exceed 100 characters.");
+
+        RuleFor(x => x.Description)
+            .MaximumLength(500).WithMessage("Feature flag description must not exceed 500 characters.");
+
+        RuleFor(x => x.Data)
+            .MaximumLength(1000).WithMessage("Feature flag data must not exceed 1000 characters.");
+    }
+}
