a4baf9af1fa08f6db0ffd8bce2054d8901bbc990
Norbert Budzynski <crosscrs@outlook.com>
2026-05-25 16:42:28 +0200

    Add unique constraint to feature flag's name + environment id

---
+++ FeatureHub.Infrastructure/Migrations/20260525143737_AddFeatureFlagUniqueNameConstraint.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FeatureHub.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFeatureFlagUniqueNameConstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_FeatureFlags_Name_EnvironmentId",
+                table: "FeatureFlags",
+                columns: new[] { "Name", "EnvironmentId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_FeatureFlags_Name_EnvironmentId",
+                table: "FeatureFlags");
+        }
+    }
+}
