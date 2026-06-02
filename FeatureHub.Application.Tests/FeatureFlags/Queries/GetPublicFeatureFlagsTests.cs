709e4133339ac3bc88e62c911be9a2cdf41ef094
Norbert Budzynski <crosscrs@outlook.com>
2026-06-02 11:57:30 +0200

    Add missing GetPublicFeatureFlags test

--- FeatureHub.Application.Tests/FeatureFlags/Queries/GetPublicFeatureFlagsTests.cs
+++ FeatureHub.Application.Tests/FeatureFlags/Queries/GetPublicFeatureFlagsTests.cs
@@ -72,6 +72,36 @@ public class GetPublicFeatureFlagsTests
     }
 
     [Fact]
+    public async Task GetPublicFeatureFlags_ShouldReturnFalseFeatureFlagsThatHaveData_WhenEnvironmentExists()
+    {
+        var environments = new List<Domain.Entities.Environment>();
+        var featureFlags = new List<FeatureFlag>();
+
+        environments.AddRange([
+            new() { Id = 1, ProjectId = 1, Name = "Production", Token = "deadbeef000000000000000000000042", FeatureFlags = featureFlags },
+        ]);
+        var environmentsMockDbSet = environments.BuildMockDbSet();
+
+        featureFlags.AddRange([
+            new() { Id = 1, EnvironmentId = 1, Name = "FeatureA", Value = true, IsActive = true },
+            new() { Id = 2, EnvironmentId = 1, Name = "FeatureB", Value = true, IsActive = true },
+            new() { Id = 3, EnvironmentId = 1, Name = "FeatureC", Value = false, Data = "Some data", IsActive = true },
+        ]);
+        var featureFlagsMockDbSet = featureFlags.BuildMockDbSet();
+
+        _mockContext.Setup(c => c.Environments).Returns(environmentsMockDbSet.Object);
+        _mockContext.Setup(c => c.FeatureFlags).Returns(featureFlagsMockDbSet.Object);
+
+        var handler = new GetPublicFeatureFlagsHandler(_mockContext.Object);
+        var query = new GetPublicFeatureFlags("deadbeef000000000000000000000042", null);
+
+        var result = await handler.ExecuteAsync(query, CancellationToken.None);
+
+        Assert.NotNull(result);
+        Assert.Equal(3, result.Count());
+    }
+
+    [Fact]
     public async Task GetPublicFeatureFlags_ShouldThrowNotFoundException_WhenEnvironmentDoesNotExist()
     {
         var environments = new List<Domain.Entities.Environment>();
